233dd58dc400af85a730ea3d5372d2bf13eb3179
Varatharuban B <developer-at-398378596368>
2020-12-25 21:11:26 +0800

    Spring boot application with PayPal integration

---
+++ src/main/java/com/rubanelearning/payments/SpringBootPaypalApplication.java
@@ -0,0 +1,13 @@
+package com.rubanelearning.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubanelearning/payments/SpringBootPaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubanelearning.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
